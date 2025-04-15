87bf9631afe2371fc33b34f78e6ef091f9eccc76
Uktasri Doki <uktasri_doki.sias22@krea.ac.in>
2025-04-15 23:48:14 +0530

    this is the modified asynclang file

--- asynclang.rkt
+++ asynclang.rkt
@@ -1,13 +1,15 @@
 #lang racket
-
 (require "./lang.rkt" 
          "./event-loop.rkt"
          "./plist.rkt")
-(require "lang.rkt")
+(provide run interp/async AsyncFunc AwaitC PromiseC AsyncFunV PromiseV AppC
+         PromiseV? AsyncFunV? AsyncFunc? AwaitC? PromiseC? AppC?)
+
 ;; Extended AST nodes
 (struct AsyncFunc (args body) #:transparent)
 (struct AwaitC (expr) #:transparent)
 (struct PromiseC (executor) #:transparent)
+(struct AppC (fexpr args) #:transparent)
 
 ;; Extended value types
 (struct AsyncFunV (env args body) #:transparent)
@@ -22,21 +24,22 @@
   (match expr
     [(AsyncFunc args body)
      (values (AsyncFunV env args body) mem)]
-
+    
     [(PromiseC executor-expr)
      (let-values ([(executor-val mem1) (interp/async evl env mem executor-expr)])
        (match executor-val
-         [(FunV _ argsym body) #:when (equal? argsym '(resolve reject))
-          (let* ([p (promise evl (λ (resolve reject)
-                        (let* ([resolve-fun (FunV env 'resolve (λ (v) (resolve v)))]
-                               [reject-fun (FunV env 'reject (λ (v) (reject v)))])
-                          (interp/async evl 
-                                       (extend-env env '(resolve reject) (list resolve-fun reject-fun))
-                                       mem1
-                                       body))))])
+         [(FunV fenv argsym body)
+          (let* ([p (promise evl 
+                             (λ (resolve reject)
+                               (let ([res-fun (FunV empty-env 'v (ApplyC (IdC 'resolve) (IdC 'v)))]
+                                     [rej-fun (FunV empty-env 'v (ApplyC (IdC 'reject) (IdC 'v)))])
+                                 (interp/async evl 
+                                              (extend-env fenv '(resolve reject) (list res-fun rej-fun))
+                                              mem1
+                                              body))))])
             (values (PromiseV p) mem1))]
-         [_ (error "Promise executor must be a function (resolve, reject) => ...")]))]
-
+         [_ (error "Promise executor must be a function")]))]
+    
     [(AwaitC expr)
      (let-values ([(promise-val mem1) (interp/async evl env mem expr)])
        (match promise-val
@@ -44,23 +47,53 @@
           (let ([result-p (promise-then! p (λ (val) val))])
             (values (PromiseV result-p) mem1))]
          [_ (error "Await expects a promise")]))]
-
-    [(ApplyC fexpr args)
+    
+    ;; Handle our custom multi-argument AppC
+    [(AppC fexpr args)
      (let*-values ([(fval mem1) (interp/async evl env mem fexpr)]
                    [(arg-vals mem2) (interp*/async evl env mem1 args)])
        (match fval
          [(AsyncFunV fenv fargs body)
-          (let ([p (promise evl (λ (resolve reject)
-                       (evl-schedule evl (λ (_)
-                         (let-values ([(result mem3) 
-                                       (interp/async evl 
-                                                     (extend-env fenv fargs arg-vals)
-                                                     mem2
-                                                     body)])
-                           (resolve result))))))]) 
+          (let ([p (promise evl
+                            (λ (resolve reject)
+                              (evl-schedule evl (λ (_)
+                                (let-values ([(result mem3) 
+                                              (interp/async evl 
+                                                          (extend-env fenv fargs arg-vals)
+                                                          mem2
+                                                          body)])
+                                  (resolve result))))))])
             (values (PromiseV p) mem2))]        
-         [_ (interp/async evl env mem (ApplyC fexpr args))]))] 
-
+         [(FunV fenv argsym body)
+          (if (symbol? argsym) 
+              ;; Classic function with single argument
+              (interp (extend fenv argsym (car arg-vals)) mem2 body)
+              ;; Assuming list args aren't supported in base lang.rkt
+              (error "Function argument mismatch"))]
+         [_ (error "Cannot apply non-function")]))]
+    
+    ;; Handle standard single-argument ApplyC from lang.rkt
+    [(ApplyC fexpr vexpr)
+     (let*-values ([(fval mem1) (interp/async evl env mem fexpr)]
+                   [(vval mem2) (interp/async evl env mem1 vexpr)])
+       (match fval
+         [(AsyncFunV fenv fargs body)
+          (if (= (length fargs) 1)
+              (let ([p (promise evl
+                                (λ (resolve reject)
+                                  (evl-schedule evl (λ (_)
+                                    (let-values ([(result mem3) 
+                                                  (interp/async evl 
+                                                              (extend fenv (car fargs) vval)
+                                                              mem2
+                                                              body)])
+                                      (resolve result))))))])
+                (values (PromiseV p) mem2))
+              (error "AsyncFunc expected ~a args but got 1" (length fargs)))]
+         [(FunV fenv argsym body)
+          (interp (extend fenv argsym vval) mem2 body)]
+         [_ (error "Cannot apply non-function")]))]
+         
     [_ (interp env mem expr)])) 
 
 ;; Helper for multiple expressions
@@ -78,6 +111,4 @@
      (let-values ([(result mem) (interp/async evl empty-env '() expr)])
        (if (PromiseV? result)
            (PromiseV-p result)
-           (promise evl (λ (resolve _) (resolve result))))))))
-
-
+           (promise evl (λ (resolve reject) (resolve result))))))))
